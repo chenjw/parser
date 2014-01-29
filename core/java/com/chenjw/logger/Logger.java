e226f18be12f54d13ed4d28b5fdea96cddf71c41
chenjw <chenjw@chenjw-ubuntu>
2014-01-29 22:39:42 +0800

    11

--- core/java/com/chenjw/logger/Logger.java
+++ core/java/com/chenjw/logger/Logger.java
@@ -1,6 +1,5 @@
 package com.chenjw.logger;
 
-
 /**
  * 日志包装
  * 
@@ -8,81 +7,80 @@ package com.chenjw.logger;
  * 
  */
 public class Logger {
-	
-	public static Logger getLogger(Class<?> clazz) {
-		return new Logger();
-	}
+    private org.apache.log4j.Logger logger;
 
-	public static Logger getLogger(String name) {
-		return new Logger();
-	}
-	
-	public Logger() {
+    public static Logger getLogger(Class<?> clazz) {
+        return new Logger(clazz);
+    }
 
-	}
+    public static Logger getLogger(String name) {
+        return new Logger(name);
+    }
 
-	public boolean isDebugEnabled() {
-		return true;
-	}
-	
-	public boolean isInfoEnabled() {
-		return true;
-	}
+    public Logger(Class<?> clazz) {
+        this.logger = org.apache.log4j.Logger.getLogger(clazz);
+    }
 
-	// /////////
+    public Logger(String name) {
+        this.logger = org.apache.log4j.Logger.getLogger(name);
+    }
 
-	public void trace(Object key) {
-		System.out.println(key);
-	}
+    public boolean isDebugEnabled() {
+        return logger.isDebugEnabled();
+    }
 
-	public void trace(Object key, Throwable cause) {
-		System.out.println(key);
-		cause.printStackTrace();
-	}
+    public boolean isInfoEnabled() {
+        return logger.isInfoEnabled();
+    }
 
-	public void debug(Object key) {
-		System.out.println(key);
-	}
+    // /////////
 
-	public void debug(Object key, Throwable cause) {
-		System.out.println(key);
-		cause.printStackTrace();
-	}
+    public void trace(Object key) {
+        logger.trace(key);
+    }
 
-	public void info(Object key) {
-		System.out.println(key);
-	}
+    public void trace(Object key, Throwable cause) {
+        logger.trace(key, cause);
+    }
 
-	public void info(Object key, Throwable cause) {
-		System.out.println(key);
-		cause.printStackTrace();
-	}
+    public void debug(Object key) {
+        logger.debug(key);
+    }
 
-	public void warn(Object key) {
-		System.out.println(key);
-	}
+    public void debug(Object key, Throwable cause) {
+        logger.debug(key, cause);
+    }
 
-	public void warn(Object key, Throwable cause) {
-		System.out.println(key);
-		cause.printStackTrace();
-	}
+    public void info(Object key) {
+        logger.info(key);
+    }
 
-	public void error(Object key) {
-		System.out.println(key);
-	}
+    public void info(Object key, Throwable cause) {
+        logger.info(key, cause);
+    }
 
-	public void error(Object key, Throwable cause) {
-		System.out.println(key);
-		cause.printStackTrace();
-	}
+    public void warn(Object key) {
+        logger.warn(key);
+    }
 
-	public void fatal(Object key) {
-		System.out.println(key);
-	}
+    public void warn(Object key, Throwable cause) {
+        logger.warn(key, cause);
+    }
 
-	public void fatal(Object key, Throwable cause) {
-		System.out.println(key);
-		cause.printStackTrace();
-	}
+    public void error(Object key) {
+        logger.error(key);
+    }
+
+    public void error(Object key, Throwable cause) {
+        logger.error(key, cause);
+    }
+
+    public void fatal(Object key) {
+        logger.fatal(key);
+    }
+
+    public void fatal(Object key, Throwable cause) {
+        logger.fatal(key, cause);
+    }
 
 }
